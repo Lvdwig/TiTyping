b23ea18c05493665e041b2558311fd5e0a4f6d45
Lvdwig <dspacziino@gmail.com>
2022-07-21 23:54:29 +0200

    Only need to check the compare the input with the word that I have to type

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
+// Define the random words, we pick the element where we have to type and where we put the words
 var randomWords = ["hello", "how", "first", "second", "keyboard", "lvdwig", 
-"futsal", "forward", "goalkeeper", "coding", "webpage", "css", "js"] 
+"futsal", "forward", "goalkeeper", "coding", "webpage", "css", "js",
+"town", "before", "after", "below", "under", "counter", "videogames",
+"mouse", "love", "cat", "dog", "friend", "brother", "family", "lights"] 
 var wordstoType = document.getElementById('textToReply')
+var wordsTyped = document.getElementById("inputW")
 
+// Get random words from the randomWords array
 function getRandomWords() {
-    for (i = 0; i < randomWords.length; i++) {
-        wordstoType.innerHTML += " " + randomWords[i];
-        console.log(randomWords[i])
+    for (i = 0; i < 15; i++) {
+        var random = Math.floor(Math.random() * randomWords.length)
+        const word = randomWords[random]
+        wordstoType.innerHTML += " " + word;
     }
 }
+
+wordsTyped.addEventListener('input', () => {
+  const getSpan = wordstoType.querySelectorAll('span')
+  const getValue = wordsTyped.value.split('')
+
+  getSpan.forEach((characterSpan, index) => {
+    const character = getValue[index]
+    if (character == null) {
+      console.log("null")
+    }
+    else if (character === characterSpan.innerText) {
+      console.log("Correct")
+    } else {
+      console.log("Incorrect")
+    }
+  })
+})
+
+
+// When Tab is pressed, it empty the content of the textArea
+document.addEventListener("keyup", function(event) {
+    if (event.code === 'Tab') {
+        wordsTyped.value = ''
+        wordstoType.innerHTML = ''
+        getRandomWords()
+    }
+});
 
 getRandomWords()
