be2cf53a170d9afa150f74804faa0c4e3dded62a
Lvdwig <dspacziino@gmail.com>
2022-07-21 18:06:17 +0200

    By now it put the words

--- js/main.js
+++ js/main.js
@@ -2,17 +2,11 @@ var randomWords = ["hello", "how", "first", "second", "keyboard", "lvdwig",
 "futsal", "forward", "goalkeeper", "coding", "webpage", "css", "js"] 
 var wordstoType = document.getElementById('textToReply')
 
-function fillRandomWords() {
-    return fetch(randomWords)
-}
-
-async function getRandomWords() {
-    var words = randomWords.forEach(putRandomWords)
-    console.log(words)
-}
-
-function putRandomWords() {
-    
+function getRandomWords() {
+    for (i = 0; i < randomWords.length; i++) {
+        wordstoType.innerHTML += " " + randomWords[i];
+        console.log(randomWords[i])
+    }
 }
 
 getRandomWords()
