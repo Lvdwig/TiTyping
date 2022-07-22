c1d893122f9c2ffe7bbb7ec80280f4faca1595e0
Lvdwig <dspacziino@gmail.com>
2022-07-22 14:09:34 +0200

    Just need to mark accerts and errors

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ var randomWords = ["hello", "how", "first", "second", "keyboard", "lvdwig",
 "mouse", "love", "cat", "dog", "friend", "brother", "family", "lights"] 
 var wordstoType = document.getElementById('textToReply')
 var wordsTyped = document.getElementById("inputW")
+var counter = 0
 
 // Get random words from the randomWords array
 function getRandomWords() {
-    for (i = 0; i < 15; i++) {
+    const limitWords = 20
+    for (i = 0; i < limitWords; i++) {
         var random = Math.floor(Math.random() * randomWords.length)
         const word = randomWords[random]
         wordstoType.innerHTML += " " + word;
@@ -16,22 +18,22 @@ function getRandomWords() {
 }
 
 wordsTyped.addEventListener('input', () => {
-  const getSpan = wordstoType.querySelectorAll('span')
-  const getValue = wordsTyped.value.split('')
+    var gettypedChar = wordsTyped.value
+    var lastCharTyped = gettypedChar.charAt(gettypedChar.length -1)
+    var getWords = wordstoType.innerText
+    var currentChar = getWords.charAt(counter)
+    
+    //console.log("This is the last char typed:" + lastCharTyped)
+    //console.log("This is the char that you have to type:" + currentChar)
 
-  getSpan.forEach((characterSpan, index) => {
-    const character = getValue[index]
-    if (character == null) {
-      console.log("null")
-    }
-    else if (character === characterSpan.innerText) {
-      console.log("Correct")
+    if (lastCharTyped == currentChar) {
+        console.log("Correct!")
+        counter += 1
     } else {
-      console.log("Incorrect")
+        console.log("Incorrect")
     }
-  })
-})
 
+})
 
 // When Tab is pressed, it empty the content of the textArea
 document.addEventListener("keyup", function(event) {
