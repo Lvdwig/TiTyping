debba3245fe07f0e686bcfea5df7b72e97635dd3
Lvdwig <dspacziino@gmail.com>
2022-07-24 22:41:36 +0200

    New system to pick words

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var randomWords = ["hello", "how", "first", "second", "keyboard", "lvdwig", 
 "futsal", "forward", "goalkeeper", "coding", "webpage", "css", "js",
 "town", "before", "after", "below", "under", "counter", "videogames",
-"mouse", "love", "cat", "dog", "friend", "brother", "family", "lights"] 
+"mouse", "love", "cat", "dog", "friend", "brother", "family", "lights"]
 var wordstoType = document.getElementById('textToReply')
 var wordsTyped = document.getElementById("inputW")
 var counterChar = 0
@@ -10,46 +10,60 @@ var counterScore = 0
 
 // Get random words from the randomWords array
 function getRandomWords() {
-    const limitWords = 20
+    const limitWords = 5
     for (i = 0; i < limitWords; i++) {
         var random = Math.floor(Math.random() * randomWords.length)
-        const word = randomWords[random]
-        wordstoType.innerHTML += " " + word;
+        const wordPicked = randomWords[random]
+        var word = document.createElement("div")
+        wordstoType.appendChild(word)
+        for (var i = 0; i < wordPicked.length; i++) {
+          var letter = document.createElement("p")
+          letter.innerHTML = wordPicked[i]
+          console.log(letter.innerText)
+          word.appendChild(letter)
+          wordstoType.innerHTML = letter.innerText
+        }
+      if (i < limitWords -1) {
+        word.innerHTML += " "
+      }
     }
 }
 
 wordsTyped.addEventListener('input', () => {
     var gettypedChar = wordsTyped.value
     var lastCharTyped = gettypedChar.charAt(gettypedChar.length -1)
-    var getWords = wordstoType.innerText
+    var getWords = wordstoType.value
     var currentChar = getWords.charAt(counterChar)
-    
     if (lastCharTyped == currentChar) {
         counterChar += 1
         counterScore += 1
-        if (counterScore <= 0) {
-          document.getElementById("score").innerHTML = "Score 0"
-      } else {
-        document.getElementById("score").innerHTML = "Score " + counterScore
-          currentChar.classList.add("correct")
-      }
-      } else {
-        counterScore -= 1
-        if (counterScore <= 0) {
+      if (counterScore <= 0) {
           document.getElementById("score").innerHTML = "Score 0"
       } else {
           document.getElementById("score").innerHTML = "Score " + counterScore
-          currentChar.classList.add("incorrect")
+      }
+    } else {
+        counterScore -= 1
+      if (counterScore <= 0) {
+          document.getElementById("score").innerHTML = "Score 0"
+      } else {
+          document.getElementById("score").innerHTML = "Score " + counterScore
+          
       }
     }
-
+  if (gettypedChar == getWords) {
+    wordstoType.innerHTML = ""
+    wordsTyped.value = ""
+    getRandomWords()
+  }
 })
 
 // When Tab is pressed, it empty the content of the textArea
-document.addEventListener("keyup", function(event) {
+wordsTyped.addEventListener("keydown", function(event) {
     if (event.code === 'Tab') {
         wordsTyped.value = ''
         wordstoType.innerHTML = ''
+        event.preventDefault()
         getRandomWords()
         counterScore = 0
         counterChar = 0
