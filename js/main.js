df043281f39e6554879560ea548528b7593d6bb3
Lvdwig <dspacziino@gmail.com>
2023-01-06 20:33:23 +0100

    New version

--- js/main.js
+++ js/main.js
@@ -13,25 +13,47 @@ var randomWords = ["hello", "how", "first", "second", "keyboard", "lvdwig", "fut
 "with", "without", "could", "year", "month", "day", " these", "those", "say", "public", "follow", "get", "leave", "however", "change", "ask", "during",
 "well", "course", "call", "write", "take", "very", "thing", "which", "child", "son", "daughter", "just", "do", "it", "still", "already", "yet",
 "increase", "possible", "impossible", "maybe", "probably", "give", "person", "come", "fact", "same", "show", "movie", "film", "old", "while"]
+
 var wordstoType = document.getElementById('textToReply')
 var wordsTyped = document.getElementById("inputW")
+var wordPicked = ""
 var counterChar = 0
 var counterScore = 0
 var totalChars = 0
 var mustEnter = true
+var seconds = 0
+var typedChars = 0
+let startTyping = true
+var descendents = ""
+var allWords = []
+var charTotype = ""
+var lastCharTyped = ""
+var scoreDiv = document.createElement("div")
+var dark = true
 
-// Declare the limit of the words that we want to type
-const limitWords = 20
+// Declare the limit of the words that we want to typed
 
+var limitWords = document.getElementById('amountWordsV').innerHTML
 // Get random words from the randomWords array
 function getRandomWords() {
 
- for (i = 1; i <= limitWords; i++) {
-    // Generate Random number
-    var random = Math.floor(Math.random() * randomWords.length)
-    // Pick a random word of the array
-    var wordPicked = randomWords[random]
-    if (i <= limitWords - 1){
+  console.log("the color of the background is: " + document.body.style.backgroundColor)
+
+  for (i = 1; i <= limitWords; i++) {
+
+    // we call the function to pick a Word
+    pickWords()
+
+    //console.log(wordPicked)
+    for (var cw = 0; cw < allWords.length; cw++) {
+      if (wordPicked == allWords[cw] ) {
+        wordPicked = ""
+        wordPicked = pickWords()
+      } else {
+        allWords.push(wordPicked)
+      }
+    }
+    if (i < limitWords){
       wordPicked += " "
     }
     totalChars += wordPicked.length
@@ -41,6 +63,7 @@ function getRandomWords() {
     word.classList.add("word")
     // We put the words inside of the the div
     wordstoType.appendChild(word)
+  
     for (l = 0; l < wordPicked.length; l++) {
       // Create a element span to put the letters
       var letter = document.createElement("span")
@@ -48,24 +71,32 @@ function getRandomWords() {
       letter.classList.add("letter")
       // We put the letters inside the div of the words
       word.appendChild(letter)
-      // We put the value of the letter into the span
+       // We put the value of the letter into the span
       letter.innerHTML = wordPicked[l]
     }
   }
 }
 
+function pickWords() {
+  // Generate Random number
+  var random = Math.floor(Math.random() * randomWords.length)
+  // Pick a random word of the array
+  return wordPicked = randomWords[random]
+}
+
 // Declare variable to use in the future
-var lastCharTyped = ""
-var charTotype = ""
 
 wordsTyped.addEventListener('input', () => {
+
+  // we put the value to true
+  startTyping = true
   // Take the words that we typed
   var gettyped = wordsTyped.innerHTML
   // Take the last char we typed
-  var lastCharTyped = gettyped.charAt(gettyped.length -1)
+  lastCharTyped = gettyped.charAt(gettyped.length -1)
   // Take the char we have to type
   var ancestor = document.getElementById("textToReply")
-  var descendents = ancestor.getElementsByClassName("letter")
+  descendents = ancestor.getElementsByClassName("letter")
   
   if (mustEnter) {
     for (var i = 0; i < descendents.length; i++) {
@@ -74,7 +105,7 @@ wordsTyped.addEventListener('input', () => {
   }
   mustEnter = false
   //console.log(charTotype)
-
+  
   // If we type a whitespace we gonna get " " i did it like that because i get the last char, and from &
   if (lastCharTyped == ';') {
     lastCharTyped = " "
@@ -84,50 +115,27 @@ wordsTyped.addEventListener('input', () => {
     //console.log("Last char typed " + lastCharTyped)
     counterChar ++
     counterScore ++
-    document.getElementById("score").innerHTML = "Score " + counterScore 
+    //document.getElementById("score").innerHTML = "Score " + counterScore 
     wordsTyped.innerText.charAt(wordsTyped.length -1)
   } else {
     //console.log("Incorrect " + charTotype.charAt(counterChar) + " And you typed " + lastCharTyped)
     counterScore --
-    document.getElementById("score").innerHTML = "Score " + counterScore
+    //document.getElementById("score").innerHTML = "Score " + counterScore
   }
-  
-  newWords()
-  //console.log("Your Score is: " + counterScore)
-  //console.log("Char position is:  " + counterChar)
   console.log("Char to type is: " + charTotype[counterChar])
+  results()
+})
 
-});
-
-async function newWords() {
-
-  //await delay(5);
-  if (counterChar == totalChars) {
-    wordsTyped.innerHTML = " "
-    wordstoType.innerHTML = " "
-    lastCharTyped
-    charTotype = ""
-    counterChar = 0
-    counterScore = 0
-    getRandomWords()
-    console.log(wordstoType.innerText)
-    document.getElementById("score").innerHTML = "Score 0"
-    mustEnter = true
-  }
-}
 
 // When Tab is pressed, it empty the content
 wordsTyped.addEventListener("keydown", function(event) {
-  if (event.code === 'Tab') {
-    wordsTyped.innerHTML = ''
-    wordstoType.innerHTML = ''
-    lastCharTyped.innerHTML = ''
+  if (event.which === 9) {
     event.preventDefault()
-    getRandomWords()
-    counterScore = 0
-    console.log(counterScore)
     counterChar = 0
-    document.getElementById("score").innerHTML = "Score 0"
+    counterScore = 0
+    wordstoType = ""
+    wordsTyped = ""
+    getRandomWords()
   }
 });
 
@@ -140,5 +148,54 @@ wordsTyped.addEventListener(("keydown"), function(event) {
     }
   }
 });
+
+var switchTheme = document.getElementById('toggleIcon')
+switchTheme.addEventListener("click", function handleClick() {
+  dark = !dark
+  console.log(dark)
+})
+
+// Function to show the results
+function results() {
+
+  var wordConverted = wordsTyped.innerText
+  var lastWordTyped = wordConverted.slice(wordConverted.lastIndexOf(" ") + 1);
+  var lastWordtoType = wordPicked.slice(wordPicked.lastIndexOf(" ") + 1)
+  if (lastWordTyped == lastWordtoType) {
+    // We disable the div to don't allow to continue typing
+    wordsTyped.remove()
+    wordstoType.remove()
+    if (dark) {
+      darkMode()
+    } else {
+      lightMode()
+    }
+    scoreDiv.innerHTML = "The score that you got was: " + counterScore + " / " + totalChars
+    scoreDiv.style.fontSize = "2em"
+    scoreDiv.style.position = "relative"
+    scoreDiv.style.textAlign = "center"
+    console.log(scoreDiv.innerText)
+    document.body.appendChild(scoreDiv)
+  }
+  console.log(totalChars)
+}
+
+// Toggle light / dark mode
+function toggleDarkMode() {
+  var elementA = document.body
+  elementA.classList.toggle("toggle-theme")
+}
+
+function darkMode() {
+  scoreDiv.style.color = "#d79921"
+}
+
+function lightMode() {
+  scoreDiv.style.color = "#1D2021"
+}
+
+function changeIcon(icon) {
+  icon.classList.toggle('fa-moon')
+}
 
 getRandomWords()
