465f25f8e1b40821a4fd59f58ace164d326663cc
Lvdwig <dspacziino@gmail.com>
2023-01-11 01:02:51 +0100

    Finished V1

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var randomWords = ["hello", "how", "first", "second", "keyboard", "lvdwig", "fut
 "drop", "location", "session", "profile", "road", "street", "singer", "sign", "maintenance", "dark", "security", "bottle", "headphones", "case",
 "plastic", "curly", "tail", "nose", "mouth", "eyes", "eyebrows", "rainbow", "close", "open", "direction", "station", "gun", "freckles", "breakfast",
 "lunch", "dine", "launch", "rocket", "moon", "planet", "kettle", "glass", "grass", "fork", "push", "pull", "develop", "problem", "big", "around",
-"with", "without", "could", "year", "month", "day", " these", "those", "say", "public", "follow", "get", "leave", "however", "change", "ask", "during",
+"with", "without", "could", "year", "month", "day", "these", "those", "say", "public", "follow", "get", "leave", "however", "change", "ask", "during",
 "well", "course", "call", "write", "take", "very", "thing", "which", "child", "son", "daughter", "just", "do", "it", "still", "already", "yet",
 "increase", "possible", "impossible", "maybe", "probably", "give", "person", "come", "fact", "same", "show", "movie", "film", "old", "while"]
 
@@ -28,16 +28,38 @@ var descendents = ""
 var allWords = []
 var charTotype = ""
 var lastCharTyped = ""
-var scoreDiv = document.createElement("div")
+var scoreDiv = document.createElement("p")
 var dark = true
+var limitWords = document.getElementById('amountWordsV')
+var resetTest = document.createElement("p")
+var textToReply = document.getElementById("textToReply")
+var descendents = textToReply.getElementsByClassName("letter")
 
-// Declare the limit of the words that we want to typed
+// when the user puts the amount of words that he wants, we delete the previous total chars
+limitWords.addEventListener('focusout', (event) => {
+  totalChars = 0
+  wordstoType.innerHTML = ""
+  scoreDiv.innerHTML = ""
+  resetTest.innerHTML = ""
+  getRandomWords(Number(event.target.value))
+  mustEnter = true
+});
 
-var limitWords = document.getElementById('amountWordsV').innerHTML
+// call the function to show the first bunch of words
+getRandomWords(document.getElementById('amountWordsV').value)
+
+
 // Get random words from the randomWords array
-function getRandomWords() {
+function getRandomWords(limitWords) {
 
-  console.log("the color of the background is: " + document.body.style.backgroundColor)
+  // we give a 0 to this variables for the case that the user modify the words twice
+  counterScore = 0
+
+  // to control the max of the words
+  if (limitWords > 50) {
+    limitWords = 50
+    document.getElementById("amountWordsV").value = "50"
+  }
 
   for (i = 1; i <= limitWords; i++) {
 
@@ -77,6 +99,7 @@ function getRandomWords() {
   }
 }
 
+// function to pick a words from the array
 function pickWords() {
   // Generate Random number
   var random = Math.floor(Math.random() * randomWords.length)
@@ -84,62 +107,68 @@ function pickWords() {
   return wordPicked = randomWords[random]
 }
 
-// Declare variable to use in the future
-
 wordsTyped.addEventListener('input', () => {
-
-  // we put the value to true
-  startTyping = true
+  
+  //console.log("Char to type is: " + charTotype[counterChar])
   // Take the words that we typed
   var gettyped = wordsTyped.innerHTML
   // Take the last char we typed
   lastCharTyped = gettyped.charAt(gettyped.length -1)
   // Take the char we have to type
-  var ancestor = document.getElementById("textToReply")
-  descendents = ancestor.getElementsByClassName("letter")
-  
-  if (mustEnter) {
-    for (var i = 0; i < descendents.length; i++) {
-      charTotype += descendents[i].innerHTML
-    }
-  }
-  mustEnter = false
-  //console.log(charTotype)
-  
+  organizingWords()
+
   // If we type a whitespace we gonna get " " i did it like that because i get the last char, and from &
   if (lastCharTyped == ';') {
     lastCharTyped = " "
   }
   if (lastCharTyped == charTotype.charAt(counterChar)) {
-    //console.log("Char to type: " + charTotype.charAt(counterChar))
+    console.log("Correct: " + charTotype.charAt(counterChar) + " And you typed " + lastCharTyped)
     //console.log("Last char typed " + lastCharTyped)
     counterChar ++
     counterScore ++
     //document.getElementById("score").innerHTML = "Score " + counterScore 
-    wordsTyped.innerText.charAt(wordsTyped.length -1)
+    //wordsTyped.innerText.charAt(wordsTyped.length -1)
   } else {
-    //console.log("Incorrect " + charTotype.charAt(counterChar) + " And you typed " + lastCharTyped)
+    console.log("Incorrect " + charTotype.charAt(counterChar) + " And you typed " + lastCharTyped)
     counterScore --
     //document.getElementById("score").innerHTML = "Score " + counterScore
   }
-  console.log("Char to type is: " + charTotype[counterChar])
   results()
 })
+
+function organizingWords() {
+  
+  if (mustEnter) {
+    for (var i = 0; i < descendents.length; i++) {
+      charTotype += descendents[i].innerHTML
+      console.log(descendents[i].innerText)
+    }
+  }
+  mustEnter = false
+}
 
 
 // When Tab is pressed, it empty the content
 wordsTyped.addEventListener("keydown", function(event) {
-  if (event.which === 9) {
+  if (event.code === 'Tab') {
     event.preventDefault()
+    console.log()
+    scoreDiv.innerHTML = ""
+    resetTest.innerHTML = ""
+    totalChars = 0
     counterChar = 0
     counterScore = 0
-    wordstoType = ""
-    wordsTyped = ""
-    getRandomWords()
+    charTotype = ""
+    wordstoType.innerHTML = ""
+    wordsTyped.innerHTML = ""
+    getRandomWords(document.getElementById('amountWordsV').value)
+    organizingWords()
+    mustEnter = true
   }
 });
 
 // When backspace is pressed, it makes like nothing's happened
+//
 wordsTyped.addEventListener(("keydown"), function(event) {
   if (event.code === 'Backspace') {
     lastCharTyped.slice(0, -1)
@@ -163,21 +192,31 @@ function results() {
   var lastWordtoType = wordPicked.slice(wordPicked.lastIndexOf(" ") + 1)
   if (lastWordTyped == lastWordtoType) {
     // We disable the div to don't allow to continue typing
-    wordsTyped.remove()
-    wordstoType.remove()
+    wordsTyped.innerHTML = ""
+    wordstoType.innerHTML = ""
     if (dark) {
       darkMode()
     } else {
       lightMode()
     }
+
+    // giving style to the result paragraph
     scoreDiv.innerHTML = "The score that you got was: " + counterScore + " / " + totalChars
     scoreDiv.style.fontSize = "2em"
     scoreDiv.style.position = "relative"
     scoreDiv.style.textAlign = "center"
-    console.log(scoreDiv.innerText)
+
+    //giving style to the reset paragraph
+    resetTest.innerHTML = "Press Tab to reset the test"
+    resetTest.style.fontSize = "1em"
+    resetTest.style.position = "absolute"
+    resetTest.style.textAlign = "center"
+    resetTest.style.top = "550px"
+
+    // we put the elements on the body
     document.body.appendChild(scoreDiv)
+    document.body.appendChild(resetTest)
   }
-  console.log(totalChars)
 }
 
 // Toggle light / dark mode
@@ -188,14 +227,14 @@ function toggleDarkMode() {
 
 function darkMode() {
   scoreDiv.style.color = "#d79921"
+  resetTest.style.color = "#d79921"
 }
 
 function lightMode() {
   scoreDiv.style.color = "#1D2021"
+  resetTest.style.color = "#1D2021"
 }
 
 function changeIcon(icon) {
   icon.classList.toggle('fa-moon')
 }
-
-getRandomWords()
